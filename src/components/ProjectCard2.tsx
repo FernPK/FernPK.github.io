b2903256f4c1de508bac510aa590a6f12ad00e84
FernPK <pornnapat5563@gmail.com>
2023-10-01 00:47:48 +0700

    add live demo

--- src/components/ProjectCard2.tsx
+++ src/components/ProjectCard2.tsx
@@ -5,7 +5,7 @@ const ProjectCard2 = () => {
   return (
     <div className='flex justify-start items-start mx-auto border border-image-border-on-white p-4 rounded-2xl project-card sm:mb-10 mb-6 max-w-screen-xl lg:w-5/6 w-11/12'>
       <div className='sm:w-3/5 w-full self-stretch'>
-        <Image src="https://cdn.discordapp.com/attachments/1156240822644260926/1157683466142503043/BunnyJeans.png?ex=6519803f&is=65182ebf&hm=ee632703e6ddd4f4544c92813b420579c919c17131213d5432b77d6d28467c2e&" alt='Random Quote Machine' height={500} width={800} className='h-full rounded-2xl object-cover'></Image>
+        <Image src="https://cdn.discordapp.com/attachments/1156240822644260926/1157735186176422008/BunnyJeans.png?ex=6519b06a&is=65185eea&hm=9ce033edfeae850597c9af7236b274a74ba6ea15b30775aae39be91cf10f2722&" alt='Random Quote Machine' height={500} width={800} className='h-full rounded-2xl object-cover'></Image>
       </div>
       <div className='px-6 lg:w-2/5 sm:w-3/5 w-full sm:pt-0 pt-4 mx-auto self-center'>
         <div className='xl:px-4 px-0 flex flex-col justify-start items-center gap-4 2xl:mb-10 xl:mb-6 mb-2'>
@@ -13,7 +13,7 @@ const ProjectCard2 = () => {
             <h3>🐰 BunnyJeans</h3>
           </div>
           <div className='xl:text-base text-sm text-body-text-on-white text-center'>
-            <p>BunnyJeans is the doll shop website. Customers can search, filter type of doll, add to wishlist, and add to cart. The website is responsive to all devices.<br/>(having a problem about deployment)</p>
+            <p>BunnyJeans is the doll shop website. Customers can search, filter type of doll, add to wishlist, and add to cart. The website is responsive to all devices.</p>
           </div>
           <div className='flex flex-row gap-4'>
             <div className="xl:text-base text-sm text-primary px-2 py-1 rounded-lg border border-primary-light">React</div>
@@ -24,6 +24,10 @@ const ProjectCard2 = () => {
           <a className='flex flex-row justify-center items-center px-4 py-2 gap-2' href='https://github.com/FernPK/BunnyJeans' target='_blank'> 
             <i className="fa-brands fa-github"></i>
             <p className='hover:underline'>GitHub</p>
+          </a>
+          <a className='flex flex-row justify-center items-center px-4 py-2 gap-2' href='https://bunnyjeans.vercel.app' target='_blank'> 
+            <i className="fa-regular fa-paper-plane"></i>
+            <p className='hover:underline'>Live demo</p>
           </a>
         </div>
       </div>
