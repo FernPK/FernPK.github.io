8751b3ac93cd9490c8a06240b9e00460991eb264
FernPK <pornnapat5563@gmail.com>
2023-09-30 20:11:51 +0700

    hover at navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,9 +6,9 @@ const NavBar = () => {
   return (
     <nav className='flex flex-row justify-center items-center gap-8 sm:h-14 h-12 w-screen text-body-text-on-white fixed bg-white z-20 sm:text-base text-sm'>
       <a href="#home" className='hover:font-bold'>Home</a>
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="#contact">Contact</a>
+      <a href="#about" className='hover:font-bold'>About</a>
+      <a href="#projects" className='hover:font-bold'>Projects</a>
+      <a href="#contact" className='hover:font-bold'>Contact</a>
     </nav>
   )
 }
