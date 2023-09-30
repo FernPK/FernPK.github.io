a02b0c9872fef8a480dab4b2ad6c9ef8314dc336
FernPK <pornnapat5563@gmail.com>
2023-09-30 19:59:53 +0700

    use relative path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import About from '/src/app/components/About'
-import Contact from '/src/app/components/Contact'
-import Home from '/src/app/components/Home'
-import Projects from '/src/app/components/Projects'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Home from '../components/Home'
+import Projects from '../components/projects'
 
 export default function Main() {
   return (
