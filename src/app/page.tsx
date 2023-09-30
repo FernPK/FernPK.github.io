09dbd58964ae8502bb0386786dce9eaab43a2ec6
FernPK <pornnapat5563@gmail.com>
2023-09-30 19:52:11 +0700

    use absolute path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import About from '../components/About'
-import Contact from '../components/Contact'
-import Home from '../components/Home'
-import Projects from '../components/projects'
+import About from '/src/app/components/About'
+import Contact from '/src/app/components/Contact'
+import Home from '/src/app/components/Home'
+import Projects from '/src/app/components/projects'
 
 export default function Main() {
   return (
