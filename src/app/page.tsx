6333b6f98e4f1b0b7bbf4b87fe73b1f214b06f63
FernPK <pornnapat5563@gmail.com>
2023-09-30 19:52:53 +0700

    projects -> Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import About from '/src/app/components/About'
 import Contact from '/src/app/components/Contact'
 import Home from '/src/app/components/Home'
-import Projects from '/src/app/components/projects'
+import Projects from '/src/app/components/Projects'
 
 export default function Main() {
   return (
