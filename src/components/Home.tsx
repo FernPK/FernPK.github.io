6441b28f9b531be86b3169bffdedfda326a8b34d
FernPK <pornnapat5563@gmail.com>
2023-11-30 18:54:37 +0700

    change drive link

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -15,7 +15,7 @@ const Home = () => {
             <h1 className='xl:text-6xl lg:text-5xl text-4xl font-medium text-header-text'><span className='font-bold'>Front-end</span> Developer</h1>
             <p className='xl:text-xl lg:text-lg text-base text-body-text-on-white px-1 py-3'>Hi, I&apos;m Pornnapat Ketplung, an undergraduate who is interested in web development and loves learning new things </p>
             <div className='flex sm:flex-row flex-col gap-4 md:mt-0 mt-1 md:justify-start justify-center'>
-              <a className='border border-white bg-primary text-white hover:bg-primary-hover button xl:text-xl lg:text-lg text-base lg:py-2 lg:px-4' href='https://drive.google.com/file/d/1MoroMgPCYfsvHqUkxqG2EDKAq6FcCa_h/view?usp=share_link' target='_blank'>
+              <a className='border border-white bg-primary text-white hover:bg-primary-hover button xl:text-xl lg:text-lg text-base lg:py-2 lg:px-4' href='https://drive.google.com/file/d/1I4N4xGlUO3ojwpmOyAiGJHdppu-EUstB/view?usp=share_link' target='_blank'>
                 <i className="fa-solid fa-circle-arrow-down lg:text-2xl text-xl"></i>Download Resume</a>
               <a className='border border-primary bg-white text-primary hover:bg-white-hover button xl:text-xl lg:text-lg text-base' href='https://github.com/FernPK' target='_blank'>
                 <i className="fa-brands fa-github lg:text-2xl text-xl"></i>Visit my GitHub</a>
